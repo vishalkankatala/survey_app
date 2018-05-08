eb3d1668fc56dd61ff4d620e0f10f1c03a4d8699
vishalkankatala <vishal.kankatala@gmail.com>
2018-05-08 14:44:55 +0530

    grbegrbe

--- hadoop_examples/spark_examples/spark_streaming/log_processor_flume_avro.py
+++ hadoop_examples/spark_examples/spark_streaming/log_processor_flume_avro.py
@@ -24,23 +24,19 @@ if __name__ == "__main__":
 	sc.setLogLevel("ERROR")
 
 	streaming_context = StreamingContext(sc,1)
-	
+
 	flumeStream = FlumeUtils.createStream(streaming_context,"localhost",9092)
-	
+
 	request_url_rdds = flumeStream.map(lambda x:x[1]).map(extract_request_url)
-	
-	aggregated_counts = request_url_rdds.map(lambda x: x,1).reduceByKeyAndWindow(lambda x,y:x+y, lambda x,y:x-y, 300,1)	
+
+	aggregated_counts = request_url_rdds.map(lambda x: x,1).reduceByKeyAndWindow(lambda x,y:x+y, lambda x,y:x-y, 300,100)
 
 	sorted_results = aggregated_counts.transform(lambda rdd: rdd.sortBy(lambda x: x[1], False))
-	
+
 	sorted_results.pprint()
-	
+
 	streaming_context.checkpoint("/user/maria_dev/checkpoint")
-	
+
 	streaming_context.start()
-	
+
 	streaming_context.awaitTermination()
-	
-	
-	
-	
